f8a174a5342110999d1412561d27839656e49578
Roberto André Bocanegra Palacios <rabp_91@hotmail.com>
2017-11-01 21:41:40 -0500

    Inicio

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sa20AdminFrontendApp
+ * @description
+ * # sa20AdminFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sa20AdminFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
