a9be040adfaa48cf1ff00ff79a0d818553292279
rabp_ <rabp_@192.168.1.8>
2018-09-20 23:21:14 -0500

    view categories-add

---
+++ app/scripts/controllers/categories-add.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sa20AdminFrontendApp.controller:CategoriesAddCtrl
+ * @description
+ * # CategoriesAddCtrl
+ * Controller of the sa20AdminFrontendApp
+ */
+angular.module('sa20AdminFrontendApp')
+.controller('CategoriesAddCtrl', function () {
+      
+});
