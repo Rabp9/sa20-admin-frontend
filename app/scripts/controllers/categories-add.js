b33e94da9f9195525e069cd5de967202e812ac7f
rabp_ <rabp_@192.168.1.8>
2018-11-04 21:41:54 -0500

    avanzando

--- app/scripts/controllers/categories-add.js
+++ app/scripts/controllers/categories-add.js
@@ -8,6 +8,42 @@
  * Controller of the sa20AdminFrontendApp
  */
 angular.module('sa20AdminFrontendApp')
-.controller('CategoriesAddCtrl', function () {
-      
+.controller('CategoriesAddCtrl', function ($scope, categoriesService, $rootScope, $uibModalInstance, $utilsViewService) {
+    $scope.category = {};
+    $scope.tmpPath = $rootScope.pathLocation + 'tmp';
+    
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.saveCategory = function(category, boton, portadaPreview) {
+        $('#' + boton).text('Guardando...');
+        $utilsViewService.disable('#' + boton);
+       
+        if (portadaPreview !== null) {
+            category.portada = portadaPreview;
+        }
+        category.estado_id = 1;
+        categoriesService.save(category, function(data) {
+            $utilsViewService.enable('#' + boton);
+            $uibModalInstance.close(data);
+        }, function(err) {
+            $utilsViewService.enable('#' + boton);
+            $uibModalInstance.close(err.data);
+        });
+    };
+    
+    $scope.previewPortada = function(portada, errFiles) {
+        $scope.loading = true;
+        var fd = new FormData();
+        fd.append('file', portada);
+        
+        categoriesService.previewPortada(fd, function(data) {
+            $scope.portadaPreview = data.filename;
+            $scope.loading = false;
+        }, function(data) {
+            $scope.portadaPreview = null;
+            $scope.loading = false;
+        });
+    };
 });
--- app/scripts/services/categoriesservice.js
+++ app/scripts/services/categoriesservice.js
@@ -8,6 +8,21 @@
  * Factory in the sa20AdminFrontendApp.
  */
 angular.module('sa20AdminFrontendApp')
-.factory('categoriesService', function($resource, envService) {
-    return $resource(envService.getHost() + "categories/:id.json", {}, {});
+.factory('categoriesService', function ($resource, envService) {
+    return $resource(envService.getHost() + 'categories/:id.json', {}, {
+        getAdmin: {
+            method: 'GET',
+            url: envService.getHost() + 'categories/getAdmin/.json'
+        },
+        getTreeList: {
+            method: 'GET',
+            url: envService.getHost() + 'categories/getTreeList/:spacer.json'
+        },
+        previewPortada: {
+            method: 'POST',
+            url: envService.getHost() + 'categories/previewPortada/.json',
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }
+    });
 });
